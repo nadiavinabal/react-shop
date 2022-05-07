c95ca55a936034167933a1df2680a7e23f0167d1
Nadia <nadiavinabal@hotmail.com>
2022-05-07 01:42:50 -0300

    fix/hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import '@styles/Header.scss';
-const logo = '@icons/icon_menu.svg'
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import notification from '@icons/icon_shopping_cart_notification.svg';
+import Menu from './Menu';
 
 const Header = () => {
+   const [toggle, setToggle] = useState(false);
+
+   const handleToggle = () => {
+     setToggle(!toggle);
+   }
+
    return (
     <nav>
-    <img src={logo} alt="menu" className="menu" />
+    <img src={menu} alt="menu" className="menu" />
     <div className="navbar-left">
-      <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+      <img src={logo} alt="logo" className="nav-logo" />
       <ul>
         <li>
           <a href="/">All</a>
@@ -32,13 +41,14 @@ const Header = () => {
     </div>
     <div className="navbar-right">
       <ul>
-        <li className="navbar-email">platzi@example.com</li>
+        <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
         <li className="navbar-shopping-cart">
-          <img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
+          <img src={notification} alt="shopping cart" />
           <div>2</div>
         </li>
       </ul>
     </div>
+    {toggle && <Menu />}
   </nav>
    );
 }
